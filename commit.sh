74b32dc9311ef983ccbe83e122c35dda1fa2e92a
FroCode <maamar.haddouche@capgemini.com>
2025-07-25 15:44:11 +0200

    update the revert shell

--- commit.sh
+++ commit.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Usage: ./revert_merge.sh <branch_name>
-BRANCH_NAME=$1
+# Usage: ./revert_merge_from_branch.sh <source_branch>
+SOURCE_BRANCH=$1
 
-# Fetch latest changes
-git fetch origin
+# Checkout master and pull latest
+git checkout master
+git pull origin master
 
-# Get the latest merge commit on the branch
-MERGE_COMMIT=$(git log origin/$BRANCH_NAME --merges -n 1 --pretty=format:"%H")
+# Find the merge commit from the source branch
+MERGE_COMMIT=$(git log master --merges --pretty=format:"%H %s" | grep "Merge pull request" | grep "$SOURCE_BRANCH" | head -n 1 | awk '{print $1}')
 
 if [ -z "$MERGE_COMMIT" ]; then
-  echo "No merge commit found on branch $BRANCH_NAME"
+  echo "No merge commit found from branch $SOURCE_BRANCH into master."
   exit 1
 fi
 
 # Revert the merge commit
-git checkout $BRANCH_NAME
 git revert -m 1 $MERGE_COMMIT
 
 # Push the changes
-git push origin $BRANCH_NAME
+git push origin master
